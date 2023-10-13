38e1710d05c99779783234109820049a3bc98635
johnguogan <johnguogan@outlook.com>
2023-10-13 17:48:31 -0300

    final

--- app/index.tsx
+++ app/index.tsx
@@ -15,7 +15,7 @@ import {
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { MyStack } from "../components/MyStack";
-import { View } from "react-native";
+import { ImageBackground, View } from "react-native";
 
 export default function Home() {
   const CHOSEN_APP = "chosen-app";
@@ -50,26 +50,29 @@ export default function Home() {
   }, []);
 
   return (
-    <MyStack>
-      <YStack space="$1"></YStack>
-      <YStack space="$2" maxWidth={600}>
-        <H2 textAlign="center" color="#00b0f0" style={{ fontWeight: 'bold' }}>WELCOME TO</H2>
-        <Image source={require('../assets/title.png')} style={{ alignSelf: "center", height: 60, width: 322 }} />
-      </YStack>
+    <MyStack padding="$0">
+      <ImageBackground source={require('../assets/background.png')} style={{ width: '100%', height: '100%' }}>        
+        <MyStack backgroundColor={'$colorTransparent'}>
+          <YStack space="$2" maxWidth={600}>
+            <View style={{ height: 200 }}></View>
+            <Image source={require('../assets/title_white.png')} style={{ alignSelf: "center", height: 60, width: 322 }} />
+          </YStack>
 
-      <YStack space="$4">
-        <Button onPress={() => routerPush("/shiftmate")} backgroundColor="#00b0f0" color="white" fontWeight="bold">
-          TIME AND ATTENDANCE
-        </Button>
-        <Button onPress={() => routerPush("/accruabl")} backgroundColor="#00b0f0" color="white" fontWeight="bold">
-          RECRUITMENT
-        </Button>
-      </YStack>
+          <YStack space="$4">
+            <Button onPress={() => routerPush("/shiftmate")} backgroundColor="#00b0f0" color="white" fontWeight="bold">
+              TIME AND ATTENDANCE
+            </Button>
+            <Button onPress={() => routerPush("/accruabl")} backgroundColor="#ffd24e" color="black" fontWeight="bold">
+              RECRUITMENT
+            </Button>
+          </YStack>
 
-      <YStack space="$2">
-        <H4 textAlign="center" color="gray">Powered by Accruabl</H4>
-        <View style={{ height: 70 }}></View>
-      </YStack>
+          <YStack space="$2">
+            <H4 textAlign="center" color="white">Powered by Accruabl</H4>
+            <View style={{ height: 110 }}></View>
+          </YStack>
+        </MyStack>      
+      </ImageBackground>
     </MyStack>
   );
 }
